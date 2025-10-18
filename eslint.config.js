52f8b1b853a7e185d2bc303c690747dc73a7d521
Lukium <mrlukium@outlook.com>
2025-10-18 04:51:27 -0400

    Migrate ESLint config to flat format

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,10 @@
-import { FlatCompat } from '@eslint/eslintrc'
-import path from 'node:path'
-import { fileURLToPath } from 'node:url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+const { FlatCompat } = require('@eslint/eslintrc')
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 })
 
-export default [
+module.exports = [
   {
     ignores: ['lib/**', 'node_modules/**'],
   },
